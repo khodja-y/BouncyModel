d2beed191e1dfc6660b2dfeb38630a51a4da5931
yasmine-khodja <44261817+yasmine-khodja@users.noreply.github.com>
2022-07-16 13:01:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useGLTF, PresentationControls, Html, useTexture } from '@react-three/dr
 import * as THREE from 'three'
 import { useSpring } from '@react-spring/core'
 import { a } from '@react-spring/web'
+import { Button, Alert } from 'react-native'
 
 import House from './components/House2'
 import Overlay from './components/Overlay.js'
@@ -13,18 +14,27 @@ export default function App() {
   const [{ background, fill }, set] = useSpring({ background: '#ffffff', fill: '#202020' }, [])
 
   return (
-    <a.main style={{ background }}>
-      <Canvas className="canvas" dpr={[1, 2]} camera={{ position: [0, 0, 15], fov: 75 }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
-        <PresentationControls global rotation={[0, 0.3, 0]} polar={[-Math.PI / 3, Math.PI / 3]} azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-          <House />
-        </PresentationControls>
-        {/*<ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />*/}
-        {/*<Environment preset="city" />*/}
-      </Canvas>
-      <Overlay fill={fill} />
-    </a.main>
+    <>
+      <a.main style={{ background }}>
+        <Canvas className="canvas" dpr={[1, 2]} camera={{ position: [0, 0, 15], fov: 75 }}>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
+          <PresentationControls global rotation={[0, 0.3, 0]} polar={[-Math.PI / 3, Math.PI / 3]} azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+            <House />
+          </PresentationControls>
+          {/*<ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />*/}
+          {/*<Environment preset="city" />*/}
+        </Canvas>
+        <Overlay fill={fill} />
+      </a.main>
+      <Button
+        title="Suivant"
+        color="#ff0000"
+        onPress={() => {
+          window.location.replace('https://2nkzth.csb.app/')
+        }}
+      />
+    </>
   )
 }
 
